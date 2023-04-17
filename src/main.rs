3609a8d461dd6edb8a851c79d7f77d9c12e4f3ca
nayan <nayansharma1010@gmail.com>
2023-04-17 18:21:04 +0530

    read dir with xml

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::collections::HashMap;
 use std::fs::{self, File};
 use std::io;
 use std::path::Path;
 use std::time::Instant;
 use xml::reader::{EventReader, XmlEvent};
+
+fn index_document(doc_content: &str) -> HashMap<String, usize> {
+    todo!("not implemented")
+}
 
 fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
     let file = File::open(file_path)?;
@@ -18,8 +23,13 @@ fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
 }
 
 fn main() -> io::Result<()> {
-    let dir_path = "docs.gl/gl4";
+    //benchmark
     let start = Instant::now();
+    //-------------------------//
+
+    // let all_documents = HashMap::<Path, HashMap<String, usize>>::new();
+
+    let dir_path = "docs.gl/gl4";
     let dir = fs::read_dir(dir_path)?;
 
     for file in dir {
@@ -29,6 +39,8 @@ fn main() -> io::Result<()> {
     }
     let duration = start.elapsed();
 
+    //--------------------------//
+    //benchmark
     println!(
         "The program took {duration} secs",
         duration = duration.as_secs()
