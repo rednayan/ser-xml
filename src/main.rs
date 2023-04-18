d4b5bd659d84072ec9d3f8377bd7a53452dafaa8
nayan <nayansharma1010@gmail.com>
2023-04-18 15:30:09 +0530

    fix: ordeiring freqs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io;
 use std::path::Path;
 use std::time::Instant;
@@ -86,43 +86,42 @@ fn main() -> io::Result<()> {
     let start = Instant::now();
     //-------------------------//
 
-    let content = read_entire_xml_file("docs.gl/gl4/glVertexAttribDivisor.xhtml")?
-        .chars()
-        .collect::<Vec<_>>();
+    let dir_path = "docs.gl/gl4";
+    let dir = fs::read_dir(dir_path)?;
 
-    let mut tf = HashMap::<String, usize>::new();
-    for token in Lexer::new(&content) {
-        let term = token
-            .iter()
-            .map(|x| x.to_ascii_uppercase())
-            .collect::<String>();
-        if let Some(freq) = tf.get_mut(&term) {
-            *freq += 1;
-        } else {
-            tf.insert(term, 1);
+    for file in dir {
+        let file_path = file?.path();
+        let content = read_entire_xml_file(&file_path)?;
+        println!("{file_path:?} -> {size}", size = content.len());
+        let content = read_entire_xml_file(&file_path)?
+            .chars()
+            .collect::<Vec<_>>();
+
+        let mut tf = HashMap::<String, usize>::new();
+        for token in Lexer::new(&content) {
+            let term = token
+                .iter()
+                .map(|x| x.to_ascii_uppercase())
+                .collect::<String>();
+            if let Some(freq) = tf.get_mut(&term) {
+                *freq += 1;
+            } else {
+                tf.insert(term, 1);
+            }
         }
 
         let mut stats = tf.iter().collect::<Vec<_>>();
         stats.sort_by_key(|(_, f)| *f);
         stats.reverse();
-        for (t, f) in tf.iter().take(10) {
-            println!("{t} => {f}");
+
+        for (t, f) in stats.iter().take(20) {
+            println!("    {t} => {f}");
         }
     }
-
-    // let all_documents = HashMap::<Path, HashMap<String, usize>>::new();
-    // let dir_path = "docs.gl/gl4";
-    // let dir = fs::read_dir(dir_path)?;
-
-    // for file in dir {
-    //     let file_path = file?.path();
-    //     let content = read_entire_xml_file(&file_path)?;
-    //     println!("{file_path:?} -> {size}", size = content.len());
-    // }
 
     //--------------------------//
     //benchmark
     let duration = start.elapsed();
-    println!("benchmark: {duration} secs ", duration = duration.as_secs());
+    println!("benchmark: {duration} ms ", duration = duration.as_millis());
     Ok(())
 }
