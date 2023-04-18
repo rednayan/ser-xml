06b6e75d74fb53c817e3075a1a0e8967e20658d8
nayan <nayansharma1010@gmail.com>
2023-04-18 14:59:54 +0530

    frequency table

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::fs::{self, File};
+use std::fs::File;
 use std::io;
 use std::path::Path;
 use std::time::Instant;
@@ -21,24 +21,37 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    fn chop(&mut self, n: usize) -> &'a [char] {
+        let token = &self.content[0..n];
+        self.content = &self.content[n..];
+        token
+    }
+
+    fn chop_while<P>(&mut self, mut predicate: P) -> &'a [char]
+    where
+        P: FnMut(&char) -> bool,
+    {
+        let mut n = 0;
+        while n < self.content.len() && predicate(&self.content[n]) {
+            n += 1;
+        }
+        self.chop(n)
+    }
+
     fn next_token(&mut self) -> Option<&'a [char]> {
         self.trim_left();
         if self.content.len() == 0 {
             return None;
         }
 
-        if self.content[0].is_ascii_alphabetic() {
-            let mut n = 0;
-            while n < self.content.len() && self.content[n].is_ascii_alphanumeric() {
-                n += 1;
-            }
-
-            let token = &self.content[0..n];
-            self.content = &self.content[n..];
-            return Some(token);
+        if self.content[0].is_numeric() {
+            return Some(self.chop_while(|x| x.is_numeric()));
         }
 
-        todo!()
+        if self.content[0].is_alphabetic() {
+            return Some(self.chop_while(|x| x.is_alphanumeric()));
+        }
+        return Some(self.chop(1));
     }
 }
 
@@ -77,8 +90,24 @@ fn main() -> io::Result<()> {
         .chars()
         .collect::<Vec<_>>();
 
+    let mut tf = HashMap::<String, usize>::new();
     for token in Lexer::new(&content) {
-        println!("{token}", token = token.iter().collect::<String>());
+        let term = token
+            .iter()
+            .map(|x| x.to_ascii_uppercase())
+            .collect::<String>();
+        if let Some(freq) = tf.get_mut(&term) {
+            *freq += 1;
+        } else {
+            tf.insert(term, 1);
+        }
+
+        let mut stats = tf.iter().collect::<Vec<_>>();
+        stats.sort_by_key(|(_, f)| *f);
+        stats.reverse();
+        for (t, f) in tf.iter().take(10) {
+            println!("{t} => {f}");
+        }
     }
 
     // let all_documents = HashMap::<Path, HashMap<String, usize>>::new();
